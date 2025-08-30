54ac5477fe7c68b5069c5192cfa7154d7d8610ab
Mudit-Tarway <official.mudittarway@gmail.com>
2025-08-30 09:52:41 +0530

    Added the extra logic

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,6 +1,8 @@
 document.querySelectorAll(".start-quiz-button").forEach(button => {
   button.addEventListener("click", function(event) {
-    window.location.href = "description/description.html";
+    let quizType = this.getAttribute("data-quiz");
+    console.log(quizType);
+    window.location.href = `description/description.html?quiz=${quizType}`;
   });
 });
 
